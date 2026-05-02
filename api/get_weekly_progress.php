970d95986e104ad0c6006ffe041de9ff534f26c9
Tanjil Sarkar <im.tanjilsarkar@gmail.com>
2026-05-02 21:51:05 +0600

    Refactor get_weekly_progress to use demo data
    Removed user authentication and database query for weekly progress.

--- api/get_weekly_progress.php
+++ api/get_weekly_progress.php
@@ -1,24 +1,10 @@
 <?php
 header('Content-Type: application/json');
 session_start();
-include_once __DIR__ . "/../database/connection.php";
 
-if(!isset($_SESSION['user_id'])) {
-    echo json_encode(['error' => 'Not logged in']);
-    exit();
-}
+$demo_progress = [
+    'hours' => [2.5, 3.0, 2.0, 3.5, 4.0, 2.5, 3.0]
+];
 
-$user_id = $_SESSION['user_id'];
-
-// Get last 7 days of study data
-$hours = [];
-for($i = 6; $i >= 0; $i--) {
-    $date = date('Y-m-d', strtotime("-$i days"));
-    $result = mysqli_fetch_assoc(mysqli_query($conn, "SELECT SUM(duration) as total 
-                                                        FROM study_sessions 
-                                                        WHERE user_id=$user_id AND study_date='$date'"));
-    $hours[] = round(($result['total'] ?? 0) / 60, 1);
-}
-
-echo json_encode(['hours' => $hours]);
+echo json_encode($demo_progress);
 ?>
